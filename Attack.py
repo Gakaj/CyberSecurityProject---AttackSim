f27009bc2ef3730b0007b1426c7ca19e66a7c24c
Gakaj <samgedge8@gmail.com>
2026-05-09 14:17:04 +0100

    Altered entropy.py to display if inputted password is in common_passwords.txt

---
+++ Attack.py
@@ -0,0 +1,7 @@
+#this version is before attempting to add salting. I understand passwords would likely be salted before hashing to improve security.
+#will think of way to solve this after completing attack simulation. 09.05.2026
+
+import Entropy
+
+
+def simplestBruteForce():

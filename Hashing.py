13d15d9367349ad1629d3bcd1453d5cadd12daa5
Gakaj <samgedge8@gmail.com>
2026-06-25 21:18:52 +0100

    Added command line arguments into hashing.py and renamed attack.py to main

--- Hashing.py
+++ Hashing.py
@@ -1,45 +1,43 @@
-import hashlib, os
+import hashlib, os, sys, argparse
 
-def hashing_program():
-  password = "Password"
 
-  def simpleHash(password):
-    password = list(password)
+def hashing():
+  parser = argparse.ArgumentParser(
+    description= "Hash a wordlist using sha-256"
+  )
+
+
+  parser.add_argument(
+      "-w",
+      "--wordlist",
+      required=True,
+      help="Path to wordlist"  
+  )
+
+
     
-    for i in range(len(password)):
-      password[i] = str(ord(password[i]) % len(password))
-    
-    hashed = "".join(password)
-    return hashed
-    
+
+  # -- >  """" Sha 256 program starts here """
+  def read_hash_wordlist(path):
+      path = args.wordlist
+
+      with open(path, "r", encoding="utf-8", errors="ignore") as file:
+        for line in file:
+          original = line.strip() # remove whitepsace and other problems
+          if not original: # skip if empty/errors to continue
+            continue #go to next iteration
 
 
-  def read_hash_wordlist():
-    while True:
-      path = input("Enter path to wordlist: ").strip().strip('""') # remove whitespace and other
-    #validate path
-      if os.path.isfile(path):
-        break #stop asking for path
-
-    print(ValueError("Incorrect path")) #if not correct type
-
-    with open(path, "r", encoding="utf-8", errors="ignore") as file:
-      for line in file:
-        original = line.strip() # remove whitepsace and other problems
-        if not original: # skip if empty/errors to continue
-          continue #go to next iteration
+          hashed = hashlib.sha256(original.encode()).hexdigest() # encode data and transform into sha256 hash
+          # as well as making it a hex NOT a byte and not output object.
+          yield hashed # makes the function a generator by returning an iterable - makes it more efficient as no longer held in RAM
 
 
-        hashed = hashlib.sha256(original.encode()).hexdigest() # encode data and transform into sha256 hash
-                                                              # as well as making it a hex NOT a byte and not output object.
-        yield hashed # makes the function a generator by returning an iterable - makes it more efficient as no longer held in RAM
-
-
-
-  for line in read_hash_wordlist(): #print every hashed line
+  args = parser.parse_args()
+  for line in read_hash_wordlist(args.wordlist): #print every hashed line
     print(line)
 
 if __name__ == "__main__":
-  hashing_program()
+  hashing()
 else:
   pass
